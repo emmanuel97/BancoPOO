93d817ca629ee6783e4e64912ce141b95f86ef69
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-07-18 15:33:35 -0300

    Update PessoaJ.java

--- PessoaJ.java
+++ PessoaJ.java
@@ -1,11 +1,17 @@
     
 public class PessoaJ extends Cliente{
     
-   String CNPJ,String nomeFant;
+   String CNPJ,nomeFant;
    public void setCNPJ(String cnpj){
        CNPJ=cnpj;
    }
     public void setNF(String fant){
         nomeFant=fant;
+    }
+     public String getCNPJ(){
+       return CNPJ;
+   }
+    public String getNF(){
+        return nomeFant;
     }
 }
