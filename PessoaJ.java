5f1f0cb57dd31d1e57c337b785c9db5d9c1b2e67
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-07-28 10:05:40 -0300

    Update PessoaJ.java

--- PessoaJ.java
+++ PessoaJ.java
@@ -7,4 +7,7 @@ String nomeFantasia,CNPJ;
 	public void setNomeF(String NF){
 	nomeFantasia=NF;
 	}
+	public String dadosC(){
+	return "Nome:"+nome+".\nTelefone:"+telefone+".\nemail:"+email+".\nNome de Fantasia:"+nomeFantasia+".\nCNPJ:"+CNPJ+".\n";
+	}
 }
