7cd360511442afa45468c01d036514581b21be56
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-07-28 10:05:10 -0300

    Update PessoaF.java

--- PessoaF.java
+++ PessoaF.java
@@ -4,4 +4,7 @@ String CPF;
 	public void setCPF(String Cpf){
 	this.CPF=Cpf;
 	}
+	public String dadosC(){
+		return "Nome:"+nome+".\nTelefone:"+telefone+".\nemail:"+email+".\nCPF:"+CPF+".\n";
+		}
 }
