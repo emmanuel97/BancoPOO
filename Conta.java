2d2b4c22ccbc21dadc3cd3520c663dcaf2f6bfc3
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-08-03 12:41:56 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -1,24 +1,44 @@
 
 public class Conta {
-    String numero;
+    int numero;
     boolean ativa;
     double saldo;
     
     
     
-      public void setAll(String numero,boolean ativa,double saldo){
+      public void setAll(int numero,boolean ativa,double saldo){
       this.ativa=ativa;
         this.numero=numero;
         this.saldo=saldo;
 }  
       public void deposito(double valor){
-    	  saldo=valor;
+    	if (ativa==true)  saldo+=valor;
+    	else System.out.println("A conta não esta ativa");
       }
       public double verSaldo(){
-    	  return saldo;
+    	  if (ativa==true) return saldo;
+    	  return 0;
       }
       
       public void extração(double valor){
-    	  saldo-=valor;
+    	  if (ativa==true)  saldo-=valor;
+      	else System.out.println("A conta não esta ativa");
+      }
+      public void cancelarConta(){
+    	  if (ativa==true){saldo=0;
+    	  ativa=false;}
+    	  else System.out.println("A conta já estava inativa");
+    	}
+      public void ativarConta(){
+    	  if (ativa==false) ativa=true;
+    		else System.out.println("A conta já estava ativa");
+    	}
+      public void transferir(Banco x,int numA,int numConta,double valor){
+      x.buscarA(numA).buscarContaC(numConta).deposito(valor);
+      }
+      public void situação(){
+    	  if (ativa==true)System.out.println("A conta esta ativa e o seu saldo é R$"+this.verSaldo());
+    	  else System.out.println("A conta não esta ativa");
       }
 }
+
