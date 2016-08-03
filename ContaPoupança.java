c40d2bbdb643ce0d136196f626a8ef75f629e3e3
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-08-03 12:41:23 -0300

    Update ContaPoupança.java

--- ContaPoupança.java
+++ ContaPoupança.java
@@ -1,11 +1,26 @@
 
-import java.util.Date;
+import java.util.Calendar;
 
 public class ContaPoupança extends Conta{
-Date data=new Date();
-
-public void getD(){
-    
-    System.out.println(data);
+Calendar aniversario;
+int numMes,numDia,numAno;
+double saldoInicioMes=saldo;
+public ContaPoupança(){
+	aniversario=Calendar.getInstance();
+	aniversario.add(Calendar.MONTH, 1);
 }
+public void getD(){
+System.out.println(aniversario.get(Calendar.YEAR)+aniversario.get(Calendar.MONTH)+aniversario.get(Calendar.DAY_OF_MONTH));
+}
+public void atualizarDatas(Calendar data){
+	if( data.get(Calendar.DAY_OF_MONTH)==aniversario.get(Calendar.DAY_OF_MONTH)){
+		if( data.get(Calendar.MONTH)==aniversario.get(Calendar.MONTH)){
+			if( data.get(Calendar.YEAR)==aniversario.get(Calendar.YEAR)){
+			aniversario.add(Calendar.MONTH, 1);
+			saldo+=(saldoInicioMes*0.005);
+			saldoInicioMes=saldo;
+			}
+		}
+	}
+}	
 }
