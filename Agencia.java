cac7a2382827a70becfa713633c0f720fde11959
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-08-03 12:38:49 -0300

    Update Agencia.java

--- Agencia.java
+++ Agencia.java
@@ -1,47 +1,51 @@
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Set;
 
 
 public class Agencia {
-    HashMap<String,PessoaF> clientesF = new HashMap<>();
-    HashMap<String,PessoaJ> clientesJ = new HashMap<>();
-     HashMap<String,ContCorrente> contasC = new HashMap<>();
-     HashMap<String,ContaPoupança> contasP = new HashMap<>();
-     String numero,nome,endereço,nomegerente;
-    public Agencia(String numero,String nome,String endereço,String nomegerente){
+    HashMap<String,PessoaF> clientesF = new HashMap<String,PessoaF>();
+    HashMap<String,PessoaJ> clientesJ = new HashMap<String,PessoaJ>();
+     HashMap<String,ContCorrente> contasC = new HashMap<String,ContCorrente>();
+     HashMap<String,ContaPoupança> contasP = new HashMap<String,ContaPoupança>();
+     int numero;
+     String nome,endereço,nomegerente,telefone;
+    public Agencia(int numero,String nome,String telefone,String endereço,String nomegerente){
     	this.nome=nome;
     	this.numero=numero;
     	this.endereço=endereço;
+    	this.telefone=telefone;
+    	this.nomegerente=nomegerente;
     }
     public String dadosA(){
-    	return "Nome: "+nome+".\nNumero :"+numero+ ".\nEndereço: "+endereço+ ".\nNome do Gerente :"+nomegerente+".\n";
+    	return "Numero :"+numero+".\nNome: "+nome+".\nTelefone:"+telefone+ ".\nEndereço: "+endereço+ ".\nNome do Gerente :"+nomegerente+".\n";
     }
-    public void criarClienteF(String chaveCliente,String nomeC,String telC,String emailC,String CPF){
+    public void criarClienteF(String nomeC,String telC,String emailC,String CPF){
     	
          PessoaF x=new PessoaF();
          x.setAll(nomeC, telC, emailC);;
          x.setCPF(CPF);
-         clientesF.put(chaveCliente,x); 
+         clientesF.put(nomeC,x); 
     }
-         public void criarClienteJ(String chaveCliente,String nomeC,String telC,String emailC,String NF,String CNPJ){    
+         public void criarClienteJ(String nomeC,String telC,String emailC,String NF,String CNPJ){    
     	 PessoaJ x=new PessoaJ();
          x.setAll(nomeC, telC, emailC);
          x.setCNPJ(CNPJ);
          x.setNomeF(NF);
-         clientesJ.put(chaveCliente,x); 
+         clientesJ.put(nomeC,x); 
     }
  
-    public void criarContaC(Cliente c,String chaveConta,String numConta, double limite){
+    public void criarContaC(Cliente c,int numConta, double limite){
     ContCorrente x=new ContCorrente();
             x.setAll(numConta, true, 0);
             x.setL(limite);
-            contasC.put(chaveConta,x);
+            contasC.put(String.valueOf(numConta),x);
     }
-    public void criarContaP(Cliente c,String chaveConta,String numConta){
+    public void criarContaP(Cliente c,int numConta){
             ContaPoupança x=new ContaPoupança();
            x.setAll(numConta, true, 0);
-         contasP.put(chaveConta,x);
+         contasP.put(String.valueOf(numConta),x);
        }
     
     
@@ -51,11 +55,11 @@ public class Agencia {
     public PessoaJ buscarClienteJ(String chaveCliente){
     	return clientesJ.get(chaveCliente);
     }
-    public ContCorrente buscarContaC(String chaveConta){
-    	return contasC.get(chaveConta);
+    public ContCorrente buscarContaC(int numConta){
+    	return contasC.get(numConta);
     }
-    public ContaPoupança buscarContaP(String chaveConta){
-    	return contasP.get(chaveConta);
+    public ContaPoupança buscarContaP(int numConta){
+    	return contasP.get(numConta);
     }
     public void listaClientes(){
     	System.out.println("Os clientes Pessoa Juridica são:\n");
@@ -67,5 +71,11 @@ public class Agencia {
         for(String chave: chavesF){
             System.out.println(clientesF.get(chave).dadosC()+"\n");}
     }
-  
+    public void calcPoupança(Calendar data){
+    	Set<String> chavesP = contasP.keySet();
+        for(String chave: chavesP){
+        	contasP.get(chave).atualizarDatas(data);
+        	contasP.get(chave).getD();
+        }
+    }
 }
