5b39d1f504fd96000e97428734e0e3a623d90a0c
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-08-17 12:17:38 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -1,10 +1,19 @@
-
+package Agencias;
+import Banco.Banco;
+import Clientes.PessoaF;
+import Clientes.PessoaJ;
 public class Conta {
     int numero;
     boolean ativa;
     double saldo;
-    
-    
+    PessoaF f;
+    PessoaJ j;
+    public Conta(PessoaF f){
+    	this.f=f;
+    }
+    public Conta(PessoaJ j){
+    	this.j=j;
+    }
     
       public void setAll(int numero,boolean ativa,double saldo){
       this.ativa=ativa;
@@ -15,9 +24,13 @@ public class Conta {
     	if (ativa==true)  saldo+=valor;
     	else System.out.println("A conta não esta ativa");
       }
-      public double verSaldo(){
+      public double Saldo(){
     	  if (ativa==true) return saldo;
     	  return 0;
+      }
+      public void verSaldo(){
+    	  if (ativa==true) System.out.println("o seu saldo é R$"+this.Saldo());
+    	  else System.out.println("A conta não esta ativa");
       }
       
       public void extração(double valor){
@@ -34,11 +47,20 @@ public class Conta {
     		else System.out.println("A conta já estava ativa");
     	}
       public void transferir(Banco x,int numA,int numConta,double valor){
-      x.buscarA(numA).buscarContaC(numConta).deposito(valor);
+      if(x.buscarA(numA)!=null){if(x.buscarA(numA).buscarContaC(numConta)!=null){
+    	  x.buscarA(numA).buscarContaC(numConta).deposito(valor);
+      saldo-=valor;}System.out.println("A conta não existe");}
+      else System.out.println("A agencia não existe");
       }
       public void situação(){
-    	  if (ativa==true)System.out.println("A conta esta ativa e o seu saldo é R$"+this.verSaldo());
+    	  if (ativa==true)System.out.println("A conta esta ativa e o seu saldo é R$"+this.Saldo());
     	  else System.out.println("A conta não esta ativa");
+      }
+      public PessoaF obterPF(){
+    	  return f;
+      }
+      public PessoaJ obterPJ(){
+    	  return j;
       }
 }
 
