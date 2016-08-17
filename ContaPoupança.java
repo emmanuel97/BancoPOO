cee6c3b6aee703d0a5d09bdb11caab28873b3d89
emmanuel97 <emmanuel_bulacio97@hotmail.com>
2016-08-17 12:16:13 -0300

    Update ContaPoupança.java

--- ContaPoupança.java
+++ ContaPoupança.java
@@ -1,14 +1,26 @@
-
+package Agencias;
 import java.util.Calendar;
+
+import Clientes.PessoaF;
+import Clientes.PessoaJ;
 
 public class ContaPoupança extends Conta{
 Calendar aniversario;
 int numMes,numDia,numAno;
 double saldoInicioMes=saldo;
-public ContaPoupança(){
+
+public ContaPoupança(PessoaJ j) {
+	super(j);
 	aniversario=Calendar.getInstance();
 	aniversario.add(Calendar.MONTH, 1);
 }
+
+public ContaPoupança(PessoaF f) {
+super(f);
+aniversario=Calendar.getInstance();
+aniversario.add(Calendar.MONTH, 1);
+}
+
 public void getD(){
 System.out.println(aniversario.get(Calendar.YEAR)+aniversario.get(Calendar.MONTH)+aniversario.get(Calendar.DAY_OF_MONTH));
 }
